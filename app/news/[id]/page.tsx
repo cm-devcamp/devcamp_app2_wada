ceca46715e38c240418885d519d012300b8f3306
cm-sagojo <devcamp@classmethod.jp>
2024-07-29 17:44:50 +0900

    Fix: Newsページにてdescriptionも表示されるように修正

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -28,6 +28,7 @@ export default function Page({ params }: { params: { id: string } }) {
       <div className="flex-col w-full min-h-screen max-w-5xl items-center justify-between p-6 bg-slate-50">
         <h1 className="text-4xl font-bold mb-6">News</h1>
         <h2 className="text-2xl font-bold mb-6">{news.title}</h2>
+        <p>{news.description}</p>
       </div>
       <Footer />
     </main>
